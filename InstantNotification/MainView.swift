1f2343aaf15ec97ae95a99fa1038288d8d8af37f
S-maruyama0223 <wanshanxiangtailang0223@gmail.com>
2021-03-04 22:31:31 +0900

    Xibでセルに反映

--- InstantNotification/MainView.swift
+++ InstantNotification/MainView.swift
@@ -8,21 +8,27 @@
 
 import UIKit
 
-class MainView: UIView {
+class MainView: UIView, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var taskTextFiled: UITextField!
+    var tasks = [TaskCellRecord]()
+    @IBOutlet weak var taskTextField: UITextField!
     @IBOutlet weak var hourTextField: UITextField!
     @IBOutlet weak var minuteTextField: UITextField!
     @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var remindTableView: UITableView!
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         loadNib()
+        remindTableView.delegate = self
+        remindTableView.dataSource = self
     }
 
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         loadNib()
+        remindTableView.delegate = self
+        remindTableView.dataSource = self
     }
 
     private func loadNib() {
@@ -31,6 +37,18 @@ class MainView: UIView {
             view.frame = self.bounds
             self.addSubview(view)
         }
+        remindTableView.register(UINib(nibName: "TaskCell", bundle: nil), forCellReuseIdentifier: "taskCell")
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell") as? TaskCell else { return UITableViewCell()}
+        cell.taskLabel.text = tasks[indexPath.row].task
+        cell.timeLabel.text = tasks[indexPath.row].time
+        return cell
     }
 
 }
