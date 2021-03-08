d1c33ad11a9326c41bd0960e5ac2b37e6ca44574
S-maruyama0223 <wanshanxiangtailang0223@gmail.com>
2021-03-08 20:36:44 +0900

    プッシュ通知完了

--- InstantNotification/AppDelegate.swift
+++ InstantNotification/AppDelegate.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import NCMB
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    let APPLICATION_KEY = "9641fd0e538574f60dd3d22ff3abfeb52492ecf25f65e79e3d663813d5f478bc"
+    let CLIENT_KEY = "1432ac554739af77ac5e463c10493f94bb4fb2e141a0bb608b3dd9fad4d4b5f3"
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if((error) != nil) {
+                print(" エラー")
+                return
+            }
+            if granted {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        }
         // Override point for customization after application launch.
         return true
     }
@@ -28,6 +44,79 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        //端末情報を扱うNCMBInstallationのインスタンスを作成
+        let installation : NCMBInstallation = NCMBInstallation.currentInstallation
+
+        //Device Tokenを設定
+        installation.setDeviceTokenFromData(data: deviceToken)
+
+        //端末情報をデータストアに登録
+        installation.saveInBackground(callback: { result in
+            switch result {
+            case .success:
+                //端末情報の登録が成功した場合の処理
+                print("保存に成功しました")
+            case let .failure(error):
+                //端末情報の登録が失敗した場合の処理
+                let errorCode = (error as! NCMBApiError).errorCode;
+                if (errorCode == NCMBApiErrorCode.duplication) {
+                    //失敗した原因がdeviceTokenの重複だった場合
+                    self.updateExistInstallation(installation: installation)
+                } else if (errorCode == NCMBApiErrorCode.noDataAvailable) {
+                    //失敗した原因がdeviceTokenの該当データが無い場合
+                    self.reRegistInstallation(installation: installation)
+                } else {
+                    //deviceTokenの該当データが無いエラー以外が返ってきた場合
+                }
+                return
+            }
+        })
+    }
+    //deviceTokenの重複で端末情報の登録に失敗した場合に上書き処理を行う
+    func updateExistInstallation(installation: NCMBInstallation) -> Void {
+        var installationQuery : NCMBQuery<NCMBInstallation> = NCMBInstallation.query
+        installationQuery.where(field: "deviceToken", equalTo: installation.deviceToken!)
+        installationQuery.findInBackground(callback: {results in
+            switch results {
+            case let .success(data):
+                //上書き保存する
+                let searchDevice:NCMBInstallation = data.first!
+                installation.objectId = searchDevice.objectId
+                installation.saveInBackground(callback: { result in
+                    switch result {
+                    case .success:
+                        //端末情報更新に成功したときの処理
+                        break
+                    case .failure:
+                        //端末情報更新に失敗したときの処理
+                        break
+                    }
+                })
+            case .failure:
+                //端末情報検索に失敗した場合の処理
+                break
+            }
+        })
+    }
+
+    //mobile backend上に該当データがない場合に新規作成処理を行う
+    func reRegistInstallation(installation: NCMBInstallation) -> Void {
+        //objectIdのみ削除し、installationが再生成されるようにする
+        installation.objectId = nil
+        //端末情報をデータストアに登録
+        installation.saveInBackground(callback: { result in
+            switch result {
+            case .success:
+                //端末情報の登録が成功した場合の処理
+                break
+            case let .failure(error):
+                //端末情報の登録が失敗した場合の処理
+                break
+            }
+        })
     }
 
 }
--- InstantNotification/MainView.swift
+++ InstantNotification/MainView.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class MainView: UIView, UITableViewDelegate, UITableViewDataSource {
 
-    var tasks = [TaskCellRecord]()
+    var finishedTasks = [TaskCellRecord(task: "終わり1", time: "12:00"), TaskCellRecord(task: "終わり2", time: "13:00")] {
+        didSet {
+            remindTableView.reloadData()
+        }
+    }
+    var tasks = [TaskCellRecord]() {
+        didSet {
+            remindTableView.reloadData()
+        }
+    }
+    var isDisplayFinisedTasks = true {
+        didSet {
+            remindTableView.reloadData()
+        }
+    }
+    
     @IBOutlet weak var taskTextField: UITextField!
     @IBOutlet weak var hourTextField: UITextField!
     @IBOutlet weak var minuteTextField: UITextField!
@@ -40,14 +55,39 @@ class MainView: UIView, UITableViewDelegate, UITableViewDataSource {
         remindTableView.register(UINib(nibName: "TaskCell", bundle: nil), forCellReuseIdentifier: "taskCell")
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // 完了表示フラグがオフならセクション一つ(完了)のみ
+        if isDisplayFinisedTasks == false {
+            return 1
+        }
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "完了"
+        default:
+            return "未完了"
+        }
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
             return tasks.count
+        } else {
+            return finishedTasks.count
+        }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // セルが取得できなければ何もしない
+        // カスタムセルが取得出来なければUITableViewセルを返す
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell") as? TaskCell else { return UITableViewCell()}
-        cell.setCell(record: tasks[indexPath.row])
+        if indexPath.section == 0 {
+            cell.setCell(record: tasks[indexPath.row])
+        } else {
+            cell.setCell(record: finishedTasks[indexPath.row])
+        }
         return cell
     }
 
